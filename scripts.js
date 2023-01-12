da777a81124f132ccf7ebe9d357a0bc346535d30
Nick Chan <>
2023-01-12 18:26:30 -0500

    tried multiple ways for value checks, none are working

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@ function calculateTotal() {
     let lenses = Number(document.querySelector('#Lenses').value);
     let coatings = Number(document.querySelector('#Coatings').value);
     let etc = Number(document.querySelector('#Etc').value);
-    let frame = Number(document.querySelector('#Frame').value);
-    let fullTotal = lenses + coatings + etc + frame;
+    let fullTotal = lenses + coatings + etc;
     document.getElementById('total').value = fullTotal;
 }
