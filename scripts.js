12655c393f42d4bbdd7feda1d5b604a609b0fb2c
Nick Chan <>
2023-01-15 18:48:41 -0500

    corrected all prices for lenses, coatings, and additionals. Also added new check in scripts for 1.74 index and additionals

--- scripts.js
+++ scripts.js
@@ -2,15 +2,29 @@ function calculateTotal() {
     let lenses = Number(document.querySelector('#Lenses').value);
     let coatings = Number(document.querySelector('#Coatings').value);
     let etc = Number(document.querySelector('#Etc').value);
-    let fullTotal = lenses + coatings + etc;
+    let fullTotal = lenses + coatings + etc +105;
     document.getElementById('total').value = fullTotal;
 }
 
 let frame = document.getElementById('Frame');
 let lens = document.getElementById('Lenses');
+let etc = document.getElementById('Etc');
 
 frame.addEventListener('change',()=>{
     if(lens.value == 100 && frame.value == 1 ||lens.value == 100 && frame.value == 2) {
         alert('Please Select a Different Index');
     }
 });
+
+etc.addEventListener('change',()=>{
+    let selectedEtc = etc.selectedIndex;
+    let selectedLens = lens.selectedIndex;
+    console.log(selectedEtc);
+    if(selectedEtc == 1 && selectedLens == 4) {
+        alert(`Index 1.74 cannot have transitions`)
+    } else if (selectedEtc == 2 && selectedLens == 4){
+            alert(`Index 1.74 cannot be tinted`)
+    }   else if (selectedEtc == 3 && selectedLens == 4){
+         alert(`Index 1.74 cannot have polarized filter`)
+}
+})
